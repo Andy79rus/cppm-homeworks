1f2d7279b5e95a2cb1aab105f24ab35c9b524c27
Andrey Serov <andy79rus@gmail.com>
2022-05-16 13:03:53 +0200

    Data check in file added

--- Task1.1/Task1.1.cpp
+++ Task1.1/Task1.1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 void shift_right(int* arr, int size)
 {
@@ -18,11 +19,12 @@ void shift_left(int* arr, int size)
 	arr[size-1] = tmp;
 }
 
-int main()
+bool check_data_in_file(std::string file)
 {
-	setlocale(LC_ALL, "Russian");
+	std::ifstream file_to_use(file);
+	std::string current_str;
 
-	std::ifstream file_to_use("in.txt");
+	bool file_is_ok = true;
 
 	if (!file_to_use.is_open())
 		{
@@ -30,24 +32,55 @@ int main()
 		}
 	else
 		{
+			while (!file_to_use.eof())
+				{
+					file_to_use >> current_str;
+					//Проверяем, что в символьной строке только цифры
+					std::regex reg("^[0-9]{1,10}$");
+					if (!(std::regex_match(current_str, reg)))
+						{
+							return false;
+						}
+
+				}
+		}
+
+	return file_is_ok;
+}
+
+int main()
+{
+	std::string filename1 = "in.txt";
+	setlocale(LC_ALL, "Russian");
+
+	if (check_data_in_file(filename1))
+	{
+		std::ifstream file_to_use(filename1);
+
+		if (!file_to_use.is_open())
+		{
+			std::cout << "Невозможно открыть файл. Файл не найден." << std::endl;
+		}
+		else
+		{
 			std::string current_str;
 			file_to_use >> current_str;
 			int size1 = std::stoi(current_str);
 			int* arr1 = new int[size1];
 			for (int i = 0; i < size1; ++i)
-				{
-					file_to_use >> current_str;
-					arr1[i] = std::stoi(current_str);
-				}
+			{
+				file_to_use >> current_str;
+				arr1[i] = std::stoi(current_str);
+			}
 
 			file_to_use >> current_str;
 			int size2 = std::stoi(current_str);
 			int* arr2 = new int[size2];
 			for (int i = 0; i < size2; ++i)
-				{
-					file_to_use >> current_str;
-					arr2[i] = std::stoi(current_str);
-				}	
+			{
+				file_to_use >> current_str;
+				arr2[i] = std::stoi(current_str);
+			}
 
 			file_to_use.close();
 
@@ -57,10 +90,10 @@ int main()
 			std::ofstream file_to_write("out.txt");
 
 			file_to_write << size2 << std::endl;
-			for (int i = 0; i < size2-1; ++i)
-				{
-					file_to_write << arr2[i] << " ";
-				}
+			for (int i = 0; i < size2 - 1; ++i)
+			{
+				file_to_write << arr2[i] << " ";
+			}
 			file_to_write << arr2[size2 - 1] << std::endl;
 
 			file_to_write << size1 << std::endl;
@@ -77,6 +110,9 @@ int main()
 			arr1 = nullptr;
 			arr2 = nullptr;
 		}
+
+	}
+	else std::cout << "Неправильные данные в файле" << std::endl;
 
 	return 0;
 }
