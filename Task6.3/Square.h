b6a092bfd6eeed38ddd26024c4f660e5ad3c502e
Andrey Serov <andy79rus@gmail.com>
2022-06-03 17:10:09 +0200

    Task 6.3

---
+++ Task6.3/Square.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Rectangle.h"
+
+// квадрат (все стороны равны, все углы равны 90)
+class Square : public Rectangle
+{
+public:
+    Square(int abcd);
+
+protected:
+    bool check() override;
+};
