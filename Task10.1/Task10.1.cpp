b279b6c2297f2701ccf9eda5628213733dd9f7f7
Andrey Serov <andy79rus@gmail.com>
2022-06-17 16:09:33 +0200

    Task 10.1

---
+++ Task10.1/Task10.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "Task10.1.h"
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name;
+	if (1 == argc)
+	{
+		std::cout << "Введите имя: ";
+		std::cin >> name;
+	}
+	else name = static_cast<std::string>(argv[1]);
+
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+
+	return 0;
+}
