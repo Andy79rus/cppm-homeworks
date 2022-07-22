acd368a04706a8ef082c38fc524bf9767b9cf410
Andrey Serov <andy79rus@gmail.com>
2022-07-22 11:32:30 +0200

    Task 8.2 fixes#5

--- Task8.2/ErrorEx.h
+++ Task8.2/ErrorEx.h
@@ -1,19 +1,8 @@
 #pragma once
 #include <stdexcept>
 
-enum class ErrorCode { Success = 0, NonPositiveSideErrorCode = 10,TriangleSidesErrorCode = 11,
-						AnglesSumNot180ErrorCode = 21, AnglesSumNot360ErrorCode = 22, UnknownErrorCode = 99 };
-
-class FigureHierarhyException : public std::domain_error
+class FigureException : public std::domain_error
 {
-private:
-	ErrorCode er;
-
-protected:
-	void set_error_code(ErrorCode);
-
 public:
-	FigureHierarhyException(ErrorCode ErrorType);
-	ErrorCode get_error_code() const;
-	const char* what() const override;
+	FigureException(std::string const& err);
 };
